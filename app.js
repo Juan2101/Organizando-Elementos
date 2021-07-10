dc2ffa01ca1114e7c67241dc7a51470cc99ad250
Juan2101 <antuezequiel01@gmail.com>
2021-07-09 22:05:45 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = 3000;
+
+app.listen(port, () => console.log("servidor creado en el puerto " + port));
+
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"views","home.html")));
+
+app.get("/register", (req,res) => res.sendFile(path.join(__dirname,"views","register.html")));
+
+app.get("/login", (req,res) => res.sendFile(path.join(__dirname,"views","login.html")));
+
+app.use(express.static("public"));
